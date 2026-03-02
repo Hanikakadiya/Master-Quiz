a404e02de896153c3e5d3a73ce41a1cc7997660b
Hani Kakadiya <hanikakadiya55@gmail.com>
2026-03-02 14:46:00 +0530

    Final attempt to fix 404 static files with whitenoise

--- MasterQuiz/settings.py
+++ MasterQuiz/settings.py
@@ -32,8 +32,8 @@ INSTALLED_APPS = [
 
 # MIDDLEWARE
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+   'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Add this right here!
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -97,8 +97,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This line allows WhiteNoise to compress and cache your CSS
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # DEFAULT PRIMARY KEY
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
